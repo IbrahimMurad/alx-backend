3e80654fb538e82b56d227c46b16dfac7b1f0507
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-22 10:06:09 +0300

    styling

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,19 +51,22 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Mapping:
         """ returns a dictionary containing the following key-value pairs:
 
-            page_size:      the length of the returned dataset page
-            page:           the current page number
-            data:           the dataset page (equivalent to return from previous task)
-            next_page:      number of the next page, None if no next page
-            prev_page:      number of the previous page, None if no previous page
-            total_pages:    the total number of pages in the dataset as an integer """
+            page_size:   the length of the returned dataset page
+            page:        the current page number
+            data:    the dataset page (equivalent to return from previous task)
+            next_page:   number of the next page, None if no next page
+            prev_page:   number of the previous page, None if no previous page
+            total_pages: the total number of pages in the dataset as an integer
+            """
         requested_data = self.get_page(page, page_size)
         return {
             "page_size": len(requested_data),
             "page": page,
             "data": requested_data,
-            "next_page": page + 1 if len(self.get_page(page + 1, page_size)) > 0 else None,
+            "next_page": page + 1
+            if len(self.get_page(page + 1, page_size)) > 0
+            else None,
             "prev_page": page - 1 if page > 1 else None,
-            "total_pages": len(self.dataset()) // page_size
-                            + (1 if len(self.dataset()) % page_size > 0 else 0)
+            "total_pages": (len(self.dataset()) // page_size)
+            + (1 if len(self.dataset()) % page_size > 0 else 0)
         }
